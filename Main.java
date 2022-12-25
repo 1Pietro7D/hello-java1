a6f5ca94775c5609f27e5edc17085199c1a1ef9f
PietroDanieli <1pie7d@gmail.com>
2022-12-25 09:00:04 +0100

    add comment for Non-primitive data types

--- Main.java
+++ Main.java
@@ -70,9 +70,30 @@ public class Main {
 // byte	    ->  1 byte	Stores whole numbers from -128 to 127
 // short	->  2 bytes	Stores whole numbers from -32'768 to 32'767
 // int	    ->  4 bytes	Stores whole numbers from -2'147'483'648 to 2'147'483'647
-// long	    ->  8 bytes	Stores whole numbers from -9'223'372'036'854'775'808 to 9'223'372'036'854'775'807 WANT L final
-// float	->  4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits WANT f final
-// double	->  8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits WANT d final
+// long	    ->  8 bytes	Stores whole numbers from -9'223'372'036'854'775'808 to 9'223'372'036'854'775'807-- WANT L final
+// float	->  4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits -- WANT f final
+// double	->  8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits -- WANT d final
 // boolean	->  1 bit	Stores true or false values
-// char	    ->  2 bytes	Stores a single character/letter or ASCII values
+// char	    ->  2 bytes	Stores a single character/letter or ASCII values WANT ''
+*/
+
+/* 
+// NON-PRIMITIVE DATA TYPES
+//
+// Non-primitive data types are called reference types because they refer to objects.
+// The main difference between primitive and non-primitive data types are:
+//
+// · Primitive types are predefined (already defined) in Java. Non-primitive types are created by 
+//   the programmer and is not defined by Java (except for String).
+//
+// · Non-primitive types can be used to call methods to perform certain operations, while primitive types cannot.
+//
+// · A primitive type has always a value, while non-primitive types can be null.
+//
+// · A primitive type starts with a lowercase letter, while non-primitive types starts with an uppercase letter.
+//
+// · The size of a primitive type depends on the data type, while non-primitive types have all the same size.
+//
+// · Examples of non-primitive types are: Strings, Arrays, Classes, Interface, etc.
+//   
 */
