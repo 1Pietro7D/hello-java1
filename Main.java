47bcade8c6859040efafe10ff3123c5daced247c
PietroDanieli <1pie7d@gmail.com>
2022-12-25 08:09:41 +0100

    add comment for Primitive Data Types

--- Main.java
+++ Main.java
@@ -1,10 +1,6 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello Java"); //hello Java
-    /* System.out.println("I am learning Java.");
-    System.out.print("working");
-    System.out.print(" progress:");
-    System.out.print(5 * 5); */
 
     // VARIABLES
 
@@ -44,3 +40,19 @@ public class Main {
     System.out.println(myBool);
   }
 }
+
+/* 
+// PRIMITIVE DATA TYPES
+//
+// A primitive data type specifies the size and type of variable values, and it has no additional methods.
+// There are eight primitive data types in Java:
+//
+// byte	    ->  1 byte	Stores whole numbers from -128 to 127
+// short	->  2 bytes	Stores whole numbers from -32,768 to 32,767
+// int	    ->  4 bytes	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+// long	    ->  8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+// float	->  4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+// double	->  8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
+// boolean	->  1 bit	Stores true or false values
+// char	    ->  2 bytes	Stores a single character/letter or ASCII values
+*/
