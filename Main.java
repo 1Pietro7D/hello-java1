0e8ac8364112b903290eda79683f33c5efb0628e
PietroDanieli <1pie7d@gmail.com>
2022-12-25 09:14:30 +0100

    add comment for CASTING

--- Main.java
+++ Main.java
@@ -4,12 +4,14 @@ public class Main {
 
     // VARIABLES
 
+    /////  STRING
     //  String - stores text, such as "Hello". String values are surrounded by double quotes
 
     final String name = "John";  // final as const
     String con = "Convert:";
     System.out.println(name);
 
+    /////  INT
     //  int - stores integers (whole numbers), without decimals, such as 123 or -123
 
     // int year = 1994;
@@ -30,6 +32,7 @@ public class Main {
     --x; // now x = 7
     System.out.println( x );
 
+    /////  FLOAT and DOUBLE 
     //  float - stores floating point numbers, with decimals, such as 19.99 or -19.99
 
     float f1 = 35e3f; //   with "e" or "E" you can use 10*n   //  int f2 = 35e3; -> error
@@ -37,7 +40,7 @@ public class Main {
     System.out.println(f1); //result: 35000.0
     System.out.println(d1); //result: -0.0012  
 
-    // convert number 
+    // CASTING
     System.out.println(con);
     double myDouble = 9.78d;
     System.out.println(myDouble);
@@ -51,13 +54,23 @@ public class Main {
     double myDouble2 = myInt2; // total convert - you can whrite without (double) because: double -> 8byte vs int -> 4byte
     System.out.println(myDouble2);
 
+    /*
+    Widening Casting (automatically) - converting a smaller type to a larger type size
+    byte -> short -> char -> int -> long -> float -> double
+
+    Narrowing Casting (manually) - converting a larger type to a smaller size type
+    double -> float -> long -> int -> char -> short -> byte
+    */
+
+    ///// CHAR
     //  char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
 
     //char myLetter = 'Dc'; will generate error
     char myLetter = 'H';
     System.out.println(myLetter);
     // FOR ASCI: https://www.w3schools.com/charsets/ref_html_ascii.asp
-
+    
+    ///// BOOLEAN
     //  boolean - stores values with two states: true or false
 
     boolean myBool = false;
