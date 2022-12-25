66062e8045ce1cde0701dc2bf91136ec4bd93ff5
PietroDanieli <1pie7d@gmail.com>
2022-12-25 07:36:47 +0100

    add some variables

--- Main.java
+++ Main.java
@@ -9,10 +9,30 @@ public class Main {
     // VARIABLES
 
     //  String - stores text, such as "Hello". String values are surrounded by double quotes
+
+    final String name = "John";  // final as const
+    System.out.println(name);
+
     //  int - stores integers (whole numbers), without decimals, such as 123 or -123
+
+    int year = 1994;
+    System.out.println(year);
+
     //  float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+
+    float height = 1.80f;
+    System.out.println(height);
+
     //  char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+
+    //char myLetter = 'Dc'; will generate error
+    char myLetter = 'H';
+    System.out.println(myLetter);
+
     //  boolean - stores values with two states: true or false
 
+    boolean myBool = false;
+    myBool = true;
+    System.out.println(myBool);
   }
 }
