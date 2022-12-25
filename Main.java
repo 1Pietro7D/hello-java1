551d77eae469fa45c71df22658d33c966eea31ce
PietroDanieli <1pie7d@gmail.com>
2022-12-25 07:16:06 +0100

    print number with multiplication

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
     System.out.println("I am learning Java.");
     System.out.print("working");
     System.out.print(" progress:");
-    System.out.print(999);
+    System.out.print(5 * 5);
   }
 }
