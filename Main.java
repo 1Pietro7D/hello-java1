54bd3fc4fbf57fd41acd4173c3621739c814a5ed
PietroDanieli <1pie7d@gmail.com>
2022-12-25 07:24:07 +0100

    comment some print and add new comment for new chapter

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Java");
-    System.out.println("I am learning Java.");
+    System.out.println("Hello Java"); //hello Java
+    /* System.out.println("I am learning Java.");
     System.out.print("working");
     System.out.print(" progress:");
-    System.out.print(5 * 5);
+    System.out.print(5 * 5); */
+
+    // VARIABLES
+
+    //  String - stores text, such as "Hello". String values are surrounded by double quotes
+    //  int - stores integers (whole numbers), without decimals, such as 123 or -123
+    //  float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+    //  char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+    //  boolean - stores values with two states: true or false
+
   }
 }
