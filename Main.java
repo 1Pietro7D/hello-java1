af12976e66cf39aa52480f031a0e87bd6c2a4889
PietroDanieli <1pie7d@gmail.com>
2022-12-25 07:12:13 +0100

    new print inline

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello Java");
     System.out.println("I am learning Java.");
+    System.out.print("working");
+    System.out.print(" progress");
   }
 }
