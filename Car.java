d837eda42d91bac53580cca2e57d20f83885b320
PietroDanieli <1pie7d@gmail.com>
2022-12-31 16:02:02 +0100

    OOP create new subclass and change some variables's Access Modifiers

--- Car.java
+++ Car.java
@@ -1,11 +1,11 @@
 //filename: Car.java
 public class Car {
-  int modelYear;
-  String modelName;
-  private String plate = "ks009";
+  protected int modelYear;
+  protected String modelName;
+  String plate = "ks009";
   static String licenze = "ok";
 
-  public Car(int year, String name) {
+  protected Car(int year, String name) {
     modelYear = year;
     modelName = name;
   }
@@ -15,4 +15,16 @@ public class Car {
     System.out.println(myCar.modelYear + " " + myCar.modelName);
     System.out.println(myCar.plate + " " + licenze);
   }
+}
+class EletricCar extends Car { // java EletricCar
+  private String typology = "Eletric";
+  
+  protected EletricCar(int year, String name) {
+    super(year, name);
+  }
+    public static void main(String[] args) {
+    EletricCar E_Car = new EletricCar(2020, "E-turbo");
+    System.out.println(E_Car.typology + E_Car.modelYear + " " + E_Car.modelName);
+  }
+ 
 }
